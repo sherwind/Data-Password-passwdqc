3ba53906e96dcf5d1e2729f2406bb961d91d7231
Sherwin Daganato <sherwin@daganato.com>
2012-04-09 11:14:13 +0800

    bump up version to 0.04

--- lib/Data/Password/passwdqc.pm
+++ lib/Data/Password/passwdqc.pm
@@ -10,7 +10,7 @@ use Any::Moose;
 use Any::Moose '::Util::TypeConstraints';
 use namespace::autoclean;
 
-our $VERSION = '0.03';
+our $VERSION = '0.04';
 
 require XSLoader;
 XSLoader::load('Data::Password::passwdqc', $VERSION);
